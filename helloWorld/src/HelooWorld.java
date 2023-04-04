b279142082f71a71a741e008f5619c0aab75e228
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-04-04 02:50:50 -0300

    Calcular Valores

---
+++ helloWorld/src/HelooWorld.java
@@ -0,0 +1,13 @@
+public class HelooWorld {
+    public static int calcularValores(int numero1, int numero2) {
+        return numero1 + numero2;
+    }
+    
+    public static void main(String[] args) {
+        int resultado = HelooWorld.calcularValores(2, 4);
+        System.out.println("O resultado foi " + resultado);
+    }
+    
+}
+
+
---
+++ helloWorld/src/br/com/raulfidelis/model/calcularValor.java
@@ -0,0 +1,9 @@
+package br.com.raulfidelis.model;
+
+public class calcularValor {
+    public static int calcularValores(int numero1, int numero2) {
+        return numero1 + numero2;
+    }
+}
+
+
