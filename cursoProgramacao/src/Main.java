e3796d097cd3487cb51c787f6ac0a04ec34005ba
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-04-14 20:49:32 -0300

    Bases do Java

--- cursoProgramacao/src/Main.java
+++ cursoProgramacao/src/Main.java
@@ -1,9 +1,27 @@
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		int y = 32;
+		double x = 10.35784;
+		String nome = "Maria";
+		int idade = 31;
+		double renda = 4000.0;
+		System.out.println(y);
+		System.out.println(x);
+		System.out.printf("%.2f%n", x);
+		System.out.printf("%.4f\n", x);
 		System.out.println("Olá Mundo!");
-
+		System.out.println("Bom dia!");
+		Locale.setDefault(Locale.US);
+		System.out.printf("%.4f\n", x);
+		System.out.println("RESULTADO = " + x + " Metros");
+		System.out.printf("RESULTADO = %.2f Metros%n", x);
+		System.out.printf("%s tem %d anos e ganha R$ %.2f Reais%n", nome, idade, renda);
+		//%s Texto, %d Inteiro, %f Ponto Flutuante, %n ou \n Quebra de Linha
+		
 	}
 
 }
